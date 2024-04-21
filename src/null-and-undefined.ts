a0631c362962cab7d59ba63fb6861e4f95b2bb37
ainao <github.com@akagawa.org>
2024-04-21 21:46:36 +0900

    test

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,8 @@
+﻿export {};
+
+let absence: null = null;
+
+//absence = 'hello';
+
+let data: undefined = undefined;
+//data = 123;
---
+++ src/void.ts
@@ -0,0 +1,8 @@
+﻿export {};
+
+function returnNothing(): void {
+  console.log('Idont return anything!');
+//  return undefined;
+}
+
+console.log(returnNothing());
